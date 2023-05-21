3401ee2b082029ce4e090cc384c62485c18b53a6
TravisIPeterson <travisianpeterson@gmail.com>
2023-05-21 16:53:08 -0700

    Complete

--- knight.rb
+++ knight.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Node
 
     attr_reader :coordinates
@@ -67,20 +69,45 @@ class KnightMoves
         [-2, -1]
     ]
 
-    attr_reader :board_array
+    attr_reader :board_array, :shortest_path
 
     def initialize(start, destination)
         @start = start
         @destination = destination
     end
 
-    def space_to_index(x, y)
-        index = x * 8 + y
+    def shortest_path
+        queue = [[@start]]
+        visited = Set.new([@start])
+
+        until queue.empty?
+            path = queue.shift
+            current = path.last
+
+            return path if current == @destination
+
+            next_moves = generate_moves(current).reject { |move| visited.include?(move)}
+
+            next_moves.each do |move|
+                visited.add(move)
+                queue << path + [move]
+            end
+        end
+
+        nil #If no path found
+    end
+
+    private
+    def generate_moves(coordinates)
+
+        x, y = coordinates
+        VALID_MOVES.map { |dx, dy| [x + dx, y + dy] }
+                   .select { |move| is_legal?(coordinates, move) }
     end
 
     def is_legal?(current, target)
-        x_diff = (current.coordinates[0] - target.coordinates[0]).abs
-        y_diff = (current.coordinates[1] - target.coordinates[1]).abs
+        x_diff = (current[0] - target[0]).abs
+        y_diff = (current[1] - target[1]).abs
         return (x_diff == 1 && y_diff == 2) || (x_diff == 2 && y_diff == 1)
     end      
 
@@ -89,6 +116,12 @@ end
 def array_fixer(array)
     array[0] = array[0].ord - 96
     array[1] = array[1].to_i
+end
+
+def convert_from_array(coordinates)
+    letter = (coordinates[0] + 96).chr.upcase
+    number = coordinates[1].to_s
+    "#{letter}#{number}"
 end
 
 board = ChessBoard.new
@@ -110,5 +143,14 @@ until board.board_array.any? { |space| space.coordinates == destination }
     destination = gets.chomp.downcase.split('')
     array_fixer(destination)
 end
-p start
-p destination
+
+path = KnightMoves.new(start, destination)
+shortest_path = path.shortest_path
+
+if shortest_path.nil?
+    puts "No path found. Oh no."
+else
+    converted_path = shortest_path.map { |coord| convert_from_array(coord) } 
+    puts "You can make it there in #{path.shortest_path.length - 1} moves!"
+    puts "Shortest path: #{converted_path.join(' -> ')}"
+end
